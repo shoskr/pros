a6c95e1997054a8d07f3b9910ae3469ad5ef15ae
overgarah <overgarah@CLS1531135779O.CHL.BNS>
2018-03-08 18:35:18 -0300

    08-03-2018 18:12

--- index.php
+++ index.php
@@ -1,16 +1,57 @@
 
 <html>
     <head>
-        <meta charset="UTF-8">
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
         <script src="bootstrap-4.0.0-alpha.6-dist/js/jquery-3.3.1.min.js"></script>
         <script type="text/javascript" src="http://code.jquery.com/jquery.js"></script>
         <title></title>
-         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> 
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> 
+
+        <style>
+            table {
+                width: 90%;
+            }
+
+            thead, tbody, tr, td, th { display: block; }
+
+            tr:after {
+                content: ' ';
+                display: block;
+                visibility: hidden;
+                clear: both;
+            }
+
+            thead th {
+                height: 30px;
+
+                /*text-align: left;*/
+            }
+
+            tbody {
+                width: 900px;
+                height: 900px;
+                overflow-y: auto;
+            }
+
+            thead {
+                /* fallback */
+            }
+
+
+            tbody td, thead th {
+                width: 14%;
+                float: left;
+            }         
+
+        </style>
+
     </head>
-    <body style=" background-color: whitesmoke">
-        
+    <body style=" background: url(img/fond.jpg); background-size: cover  ">
 
 
         <script src="bootstrap-4.0.0-alpha.6-dist/js/jquery-3.3.1.min.js" ></script>
